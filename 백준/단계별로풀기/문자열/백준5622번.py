af3182ab1a5ce3682c6635c2640e63aeeaaa43d2
[hoontaepark] <pthomj13@gmail.com>
2024-08-14 01:33:53 +0900

    5622번 해결

---
+++ 백준/단계별로풀기/문자열/백준5622번.py
@@ -0,0 +1,8 @@
+dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+n = input()
+count = 0
+for i in range(len(a)):
+    for j in dial:
+        if n[i] in j:
+            count += dial.index(j)+3
+print(count)
