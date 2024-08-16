cfd67877684cc72191847e1baf9333adaac73bcb
[hoontaepark] <pthomj13@gmail.com>
2024-08-16 18:24:32 +0900

    10798번 해결

---
+++ 백준/단계별로풀기/2차원배열/백준10798번.py
@@ -0,0 +1,22 @@
+board = [input() for i in range(5)]
+
+for i in range(15):
+    for j in range(5):
+        if i < len(board[j]):
+            print(board[j][i], end='')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
