a0df9811beb7222b386364099ad453d5799ae616
[hoontaepark] <pthomj13@gmail.com>
2024-09-12 17:32:02 +0900

    11866번 해결

---
+++ 백준/단계별로풀기/스택,큐,덱/백준11866번.py
@@ -0,0 +1,17 @@
+import sys
+from collections import deque
+
+n, k = map(int, sys.stdin.readline().split())
+yosep = []
+
+queue = deque([i for i in range(1, n+1)])
+
+while queue:
+    # k-1번 회전
+    for j in range(k-1):
+        queue.append(queue.popleft())
+    yosep.append(queue.popleft())
+
+print("<", end="")
+print(", ".join(map(str, yosep)), end="")
+print(">")
