0ec817a83570ec43ebbc20f1b80e505885a4e70e
[hoontaepark] <pthomj13@gmail.com>
2024-09-20 20:28:27 +0900

    4779번 해결

---
+++ 백준/단계별로풀기/재귀/백준4779번.py
@@ -0,0 +1,16 @@
+def cantor(n):
+    if n == 1:
+        return "-"
+
+    cantor_unit = cantor(n // 3)
+    cantor_res = cantor_unit + " " * (n // 3) + cantor_unit
+
+    return cantor_res
+
+
+while True:
+    try:
+        N = int(input())
+        print(cantor(3**N))
+    except:
+        break
