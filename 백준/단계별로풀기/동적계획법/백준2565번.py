f3366ef6f33ed7655820cb712b01bb7dc83476a6
[hoontaepark] <pthomj13@gmail.com>
2024-11-22 14:40:16 +0900

    2565번 해결

---
+++ 백준/단계별로풀기/동적계획법/백준2565번.py
@@ -0,0 +1,13 @@
+import sys
+n = int(sys.stdin.readline())
+wire = [tuple(map(int, input().split())) for _ in range(n)]
+wire.sort()
+
+dp = [1] * n
+
+for i in range(1, n):
+    for j in range(i):
+        if wire[j][1] < wire[i][1]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(n - max(dp))
