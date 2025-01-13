192a9f9ceaed3b10590b1d39571757d94654ec69
[hoontaepark] <pthomj13@gmail.com>
2025-01-13 17:56:51 +0900

    약수 구하기

---
+++ 프로그래머스/입문/대문자와 소문자.py
@@ -0,0 +1,12 @@
+def solution(my_string):
+    answer = ""
+    for i in my_string:
+        if i.islower():
+            answer += i.upper()
+        elif i.isupper():
+            answer += i.lower()
+        else:
+            answer += i
+
+    return answer
+
